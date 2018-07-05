7945040fdeb6d29fff5aa026bcdeaa2e719c82cc
John Gainfort Jr <john@realeyes.com>
2018-07-05 09:37:51 -0600

    Fixed error calculating creative duration

--- VastClient/VastClient/utils/String+seconds.swift
+++ VastClient/VastClient/utils/String+seconds.swift
@@ -29,7 +29,7 @@ extension String {
                 } else {
                     sec += val
                 }
-                return acc + val
+                return acc + sec
             })
         }
     }
--- VastClientWrapper/VastClientWrapper/VastExample.swift
+++ VastClientWrapper/VastClientWrapper/VastExample.swift
@@ -11,6 +11,7 @@ import VastClient
 
 let fw_vast_wrapper = "http://office.realeyes.com/jgainfort/replayer/fw_vast_wrapper.xml"
 let fw_typical = "http://29773.v.fwmrm.net/ad/g/1?flag=+sltp-slcb+qtcb+aeti+emcr&nw=169843&mode=live&prof=nbcsports_adobe_web&csid=nbcsports_nfl_snf_main_cam&sfid=7090576&afid=140315314&caid=nbcs_136401&vdur=600&resp=vast3&metr=7&pvrn=4980890867&vprn=5987497628;_fw_ae=&_fw_vcid2=169843:9cd22d95-7966-4a38-81f5-5de4c5;slid=mid1&tpos=0&slau=midroll&ptgt=a&tpcl=MIDROLL&maxd=120&cpsq=85&mind=120;"
+let fw_test = "http://29773.v.fwmrm.net/ad/g/1?flag=+sltp-slcb+qtcb+aeti+emcr&nw=169843&mode=live&prof=169843:nbcsports_adobe_web&csid=nbcsports_nfl_snf_main_cam&sfid=7090576&afid=140315314&caid=nbcs_145468&vdur=600&resp=vast3&metr=7&pvrn=310894571447039984&vprn=9050660999491739501;_fw_ae=&_fw_vcid2=169843:26026DA7-3BCC-4C70-8877-A06BECEA7ACA;slid=mid1&tpos=0&slau=midroll&ptgt=a&tpcl=MIDROLL&maxd=120.0&cpsq=5&mind=120.0;slid=300companion&slau=FW%20Video-300x250%7CFW%20Video-300x600&ptgt=p&flag=+cmpn&w=300&h=250&cd=300%2C250%7C300%2C600;slid=728companion&slau=FW%20Video-728x90%7CFW%20Video-970x66&ptgt=p&flag=+cmpn&w=728&h=90&cd=728%2C90%7C970%2C66;slid=728fullscreen&slau=FW%20Video-728x90-Image&ptgt=p&flag=+cmpn&w=728&h=90&cd=728%2C90%7C970%2C66;"
 
 class VastExample {
 
@@ -24,7 +25,8 @@ class VastExample {
     }
 
     private func makeVastRequest() {
-        let urlStr = fw_vast_wrapper
+//        let urlStr = fw_vast_wrapper
+        let urlStr = fw_test
         guard let url = URL(string: urlStr) else { return }
         do {
             let start = Date()
